633ebc3996b468dcf66bd50de9623d59cc505ddb
divol13 <divol13@gmail.com>
2017-11-23 20:50:51 +0300

    just create a JavaFX entry point

---
+++ src/main/java/com/divol13/lines/App.java
@@ -0,0 +1,16 @@
+package com.divol13.lines;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
